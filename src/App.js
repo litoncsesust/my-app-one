ebcd1b2bfde90de19ff5f407d5dca0c6b3250959
Mohammad Liton <litoncsesust@gmail.com>
2020-12-24 01:50:08 +0600

    showing current time by using react style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-/* import logo from './logo.svg'; */
 import './App.css';
 
 function App() {
   return (
     <div className="person">
       <h1>Hello</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
       <p>Good to see you here!</p>
     </div>
 
