2c71e35cf1222c4a6e5d05d0d7f831fa03bec986
Mohammad Liton <litoncsesust@gmail.com>
2020-12-24 18:21:15 +0600

    introduce props & state in react app

--- src/App.js
+++ src/App.js
@@ -6,15 +6,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
-  render() {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 26 },
+      { name: 'Bale', age: 29 }
+    ]
+  }
+  switchNameHandler = () => {
+    //console.log('was clicked!');
+    //this.state.persons[0].name = "Maximilions";
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 35 },
+        { name: 'Bale', age: 29 }
+      ]
+    })
+  }
 
+  render() {
     return (
       <div className="person">
         <h1>Hello</h1>
         <h2>It is {new Date().toLocaleTimeString()}.</h2>
         <p>Good to see you here!</p>
-        <Person name="Liton" age="32" />
-      </div>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} > My hobbies : Football </Person>
+      </div >
 
     );
 
